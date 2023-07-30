90b3ecefbee940219b282444136e1e371fa11ed0
Alessandro Sherhey <83360723+alessandro-sherhey@users.noreply.github.com>
2023-07-30 20:01:40 +0200

    Added password list animations

--- src/components/PasswordBox.jsx
+++ src/components/PasswordBox.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PasswordBox = ({ password }) => {
     return (
-        <div>
+        <div className='px-3 py-1'>
             { password }
         </div>
     )
--- src/components/PasswordsList.jsx
+++ src/components/PasswordsList.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PasswordBox from './PasswordBox'
+import { useAnimate, stagger, AnimatePresence } from 'framer-motion'
+import { useDispatch } from 'react-redux'
+import styles from '../styles/styles'
 
 const PasswordsList = ({ passwords }) => {
+    const [scope, animate] = useAnimate();
+
+    useEffect(() => {
+        if (passwords.length > 0) {
+            animate("*", {opacity: [0, 1]}, {delay: stagger(.05)})
+        }
+    }, [passwords])
+
     return (
-        <section>
+        <section
+            className='flex justify-around flex-wrap px-8 mt-7'
+            ref={scope}
+        >
             {
                 passwords.map(password => (
                     <PasswordBox password={password} />
