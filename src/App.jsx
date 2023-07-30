7cb633b7d4acf6a710a62d27656072d3fa362d52
Alessandro Sherhey <83360723+alessandro-sherhey@users.noreply.github.com>
2023-07-30 15:50:30 +0200

    Added `Options` component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Navbar from "./components/Navbar"
+import Options from "./components/Options"
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <main>
+        <Options />
+      </main>
     </>
   )
 }
